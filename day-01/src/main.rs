a60cfeb17a30e4e8833f765e86aff0d59ebd7b94
LuisSuall <luissuall@gmail.com>
2023-12-02 11:37:53 +0100

    Day01-part1

---
+++ day-01/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let input = include_str!("input.txt");
+    println!("{}", compute_calibration(input));
+}
+
+fn compute_calibration(input: &str) -> u32 {
+    input.lines().into_iter().map(|x| 10*first_number(x)+last_number(x)).sum()
+}
+
+fn first_number(line: &str) -> u32 {
+    let first_digit_char = line.chars().into_iter().filter(|x| x.is_digit(10)).next();
+    match first_digit_char {
+        Some(x) => x.to_digit(10).unwrap(),
+        None => 0,
+    }
+}
+
+fn last_number(line: &str) -> u32 {
+    let first_digit_char = line.chars().into_iter().filter(|x| x.is_digit(10)).last();
+    match first_digit_char {
+        Some(x) => x.to_digit(10).unwrap(),
+        None => 0,
+    }
+}
+
